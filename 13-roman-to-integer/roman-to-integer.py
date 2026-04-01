cc81903a2b491ec6d4ba27eac3f36657af1b2fe2
strifeja <79296434+strifeja@users.noreply.github.com>
2026-03-31 23:04:41 -0400

    Time: 10 ms (50.42%) | Memory: 12.3 MB (73.83%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -9,17 +9,17 @@ class Solution(object):
         for i in range(len(s)):
             if s[i] == 'I':
                 result += 1
-            if s[i] == 'V':
+            elif s[i] == 'V':
                 result += 3 if prev == 'I' else 5   
-            if s[i] == 'X':
+            elif s[i] == 'X':
                 result += 8 if prev == 'I' else 10
-            if s[i] == 'L':
+            elif s[i] == 'L':
                 result += 30 if prev == 'X' else 50
-            if s[i] == 'C':
+            elif s[i] == 'C':
                  result += 80 if prev == 'X' else 100
-            if s[i] == 'D':
+            elif s[i] == 'D':
                 result += 300 if prev == 'C' else 500
-            if s[i] == 'M':
+            elif s[i] == 'M':
                 result += 800 if prev == 'C' else 1000
             
             prev = s[i]
